d7107d39ad18b2a5d06df58851d17a8979cd33ea
Felix-ai <41853792+Felix-ai@users.noreply.github.com>
2022-03-30 16:03:50 +0300

    clean up

--- installApp.sh
+++ installApp.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 docker run -d -p $CLIENT_PORT:$DOCKER_PORT --rm -e HTTP=$DOCKER_ENV1_VALUE -e HTTPS=$DOCKER_ENV2_VALUE --name $IMAGE_REPO_NAME $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
