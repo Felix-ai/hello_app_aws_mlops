3181c438c56bea9fec119a0c1e84be165fbe655d
Felix-ai <41853792+Felix-ai@users.noreply.github.com>
2022-03-30 21:05:29 +0300

    bare

--- installApp.sh
+++ installApp.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
-sudo docker run -d -p $CLIENT_PORT:$DOCKER_PORT --rm -e HTTP=$DOCKER_ENV1_VALUE -e HTTPS=$DOCKER_ENV2_VALUE --name $IMAGE_REPO_NAME $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+sudo docker ps
