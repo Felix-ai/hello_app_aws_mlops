57eb931659106144a018cf08baa07c5fc782d336
Felix-ai <41853792+Felix-ai@users.noreply.github.com>
2022-03-30 16:49:25 +0300

    user

--- installApp.sh
+++ installApp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
-docker run -d -p $CLIENT_PORT:$DOCKER_PORT --rm -e HTTP=$DOCKER_ENV1_VALUE -e HTTPS=$DOCKER_ENV2_VALUE --name $IMAGE_REPO_NAME $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+sudo docker run -d -p $CLIENT_PORT:$DOCKER_PORT --rm -e HTTP=$DOCKER_ENV1_VALUE -e HTTPS=$DOCKER_ENV2_VALUE --name $IMAGE_REPO_NAME $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
