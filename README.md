d63486104c1dc6180f8e68007e8994a38acf4128
Felix-ai <41853792+Felix-ai@users.noreply.github.com>
2022-05-05 14:57:18 +0300

    handler function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hello app 
 AWS Lambda MLops workflow
+
+
+## Test your application locally using the runtime interface emulator
+curl -XPOST "http://localhost:8080/2015-03-31/functions/function/invocations" -d '{}'
--- app/teams_message.py
+++ app/teams_message.py
@@ -1,15 +1,14 @@
 import json
-import pymsteams
+# import pymsteams
 
 
 def lambda_handler(event, context):
-    # Create a connectorcard object with the Microsoft Webhook URL
-
-
-    # Add text to the message.
-    
-
-    # send the message.
-    return {'statusCode': 200,
-            'body': json.dumps(myTeamsMessage.send())
+    body = {"message": "Go Serverless v1.0! Your function executed successfully!",
+            "input": event
             }
+    
+    response = {"statusCode": 200,
+                "body": json.dumps(body)
+                }
+
+    return response
